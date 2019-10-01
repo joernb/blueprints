0e38200b0eb8f5b6acfc2b3ed7b9e673ad28dc3e
Jörn Buchwald <git@joernbuchwald.com>
2019-10-01 18:39:45 +0200

    simplify layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,9 @@
-import React, { ReactNode } from "react";
-import { AppBar, Box, CssBaseline, Grid } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { makeStyles, ThemeProvider } from "@material-ui/styles";
+import React, { ReactNode } from "react";
 import theme from "../theme";
-import Header from "./header";
 
-const useStyles = makeStyles(() => ({
-  appBar: {},
-}));
+const useStyles = makeStyles(() => ({}));
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const classes = useStyles();
@@ -14,15 +11,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* TODO adjust AppBar positioning, consider top margin of content, https://material-ui.com/api/app-bar/ */}
-      <AppBar position="sticky" className={classes.appBar}>
-        <Header />
-      </AppBar>
-      <Grid container wrap="nowrap">
-        <Grid item xs={12}>
-          <Box>{children}</Box>
-        </Grid>
-      </Grid>
+      {children}
     </ThemeProvider>
   );
 };
