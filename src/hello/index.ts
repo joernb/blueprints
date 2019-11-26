c5d7291cc17490bc07ebe658a245a3eb525bfa0b
Jörn Buchwald <git@joernbuchwald.com>
2019-11-26 19:15:33 +0100

    integrate aws lambda deployment

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json((req as any).apiGateway.event);
 });
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import awsServerlessExpressMiddleware from "aws-serverless-express/middleware";
 import helloMiddleware from "./hello";
 
 const app = express();
+
+// integrate middleware to bridge AWS serverless events to express middlewares
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // compose express app from express middlewares exported by modules
 app.use("/", helloMiddleware);
