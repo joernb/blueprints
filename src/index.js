1a451fcb77fdbfb68a9b3740ca03e336bf7e61ca
Jörn Buchwald <git@joernbuchwald.com>
2019-12-02 00:44:12 +0100

    integrate cors handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const helloMiddleware = require("./hello");
 
 const app = express();
+
+// enable Cross-Origin Resource Sharing
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus:
+      parseInt(process.env.CORS_OPTIONS_SUCCESS_STATUS, 10) || undefined,
+  })
+);
 
 // compose express app from express middlewares exported by modules
 app.use("/", helloMiddleware);
