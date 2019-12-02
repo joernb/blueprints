8627e599bff8cfeb161116f6cef7102d7f8a3183
Jörn Buchwald <git@joernbuchwald.com>
2019-12-02 22:07:36 +0100

    remove cache

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,17 +9,13 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
-// import { HttpLink } from "apollo-link-http";
 
-// "https://gitlab.com/api/graphql",
 if (!process.env.GATSBY_APOLLO_CLIENT_URI) {
   throw Error("Missing environment variable GATSBY_APOLLO_CLIENT_URI");
 }
 
 export const wrapRootElement = ({ element }, { options }) => {
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
     uri: process.env.GATSBY_APOLLO_CLIENT_URI,
   });
 
