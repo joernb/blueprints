780bc65d73ca04f8295ad17e076b4dacf963fffe
Jörn Buchwald <git@joernbuchwald.com>
2019-12-03 13:38:12 +0100

    add s3 deployment

--- aws-cdk/stack.ts
+++ aws-cdk/stack.ts
@@ -1,7 +1,20 @@
 import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
 export class Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      // TODO configure build output folder
+      sources: [s3deploy.Source.asset("./public")],
+      destinationBucket: websiteBucket,
+    });
   }
 }
